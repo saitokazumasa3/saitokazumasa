fcef3c850879fe0235d7d5fb391cf2486e820786
Saito Kazumasa <kotobukisyo@gmail.com>
2024-12-21 23:55:36 +0900

    プロジェクト作成

---
+++ src/main/java/com/saitokazumasa/saitokazumasa/SaitokazumasaApplication.java
@@ -0,0 +1,13 @@
+package com.saitokazumasa.saitokazumasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaitokazumasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaitokazumasaApplication.class, args);
+    }
+
+}
