ad102fa4cd23f0e76e11c5bfc9eb445d851aeabb
Saito Kazumasa <kotobukisyo@gmail.com>
2024-12-22 08:47:39 +0900

    build_tailwindcss.shを少し改善

--- src/main/bin/build_tailwindcss.sh
+++ src/main/bin/build_tailwindcss.sh
@@ -3,4 +3,4 @@
 INPUT_CSS_PATH="./src/main/resources/static/css/tailwindcss.css"
 OUTPUT_CSS_PATH="./src/main/resources/static/css/tailwindcssout.css"
 
-npx tailwindcss -i $INPUT_CSS_PATH -o $OUTPUT_CSS_PATH
+npx tailwindcss -i $INPUT_CSS_PATH -o $OUTPUT_CSS_PATH --watch
